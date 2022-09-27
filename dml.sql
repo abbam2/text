d68c82b87701ba96ada64f43997a7b5de515cfea
Abba Marc-André Isaac <abba@MacBook-Air-de-Abba.local>
2022-09-27 16:57:41 +0000

    text

---
+++ dml.sql
@@ -0,0 +1,43 @@
+INSERT INTO Products
+(
+Product_id
+Product_Name
+Category
+Price
+)
+VALUES
+(
+P01,P02
+Samsung Galaxy S20,ASUS Notebook
+Smarthphone,PC
+3299,4599
+);
+
+INSERT INTO Customer(
+Customer_id
+Customer_Name
+Customer_Tel
+)
+VALUES
+(
+C01,C02
+Ali,Asma
+71321009,77345823
+);
+
+INSERT INTO Orders(
+Customer_Id
+Product_Id
+OrderDate
+Quantity
+Total_amount
+)
+VALUES
+(
+CO1,CO2
+PO2,PO1,
+NULL,28/05/2020
+2,1
+9198,3299
+)
+Footer
